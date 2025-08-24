16497e516db41777e5a09f866e404a591c92f600
MariuxiJ <mj.shingrec@uea.edu.ec>
2025-08-24 07:02:33 -0500

    fin-proyecto-programacio

--- src/main/java/com/mycompany/tarea1/Tarea1.java
+++ src/main/java/com/mycompany/tarea1/Tarea1.java
@@ -11,6 +11,6 @@ package com.mycompany.tarea1;
 public class Tarea1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Marixi Shingre!");
     }
 }
